ba20ff460b1df3cc1d5a739333a14baa6e58dd3e
Peter Sanford <psanford@sanford.io>
2022-02-03 16:07:21 -0800

    Fix whitespace

--- openat/openat.bpf.c
+++ openat/openat.bpf.c
@@ -5,19 +5,19 @@
 #include "common.h"
 #include "bpf_helpers.h"
 
-#define __NR_openat				257
+#define __NR_openat        257
 
 
 char __license[] SEC("license") = "Dual MIT/GPL";
 
 #define PATH_MAX 4096
 struct event_t {
-	u32 pid;
-	char str[PATH_MAX];
+  u32 pid;
+  char str[PATH_MAX];
 };
 
 struct event_tt {
-	u32 pid;
+  u32 pid;
 };
 
 struct bpf_map_def SEC("maps") tmp_storage_map = {
@@ -28,7 +28,7 @@ struct bpf_map_def SEC("maps") tmp_storage_map = {
 };
 
 struct {
-	__uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
+  __uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
 } events SEC(".maps");
 
 SEC("raw_tracepoint/sys_enter")
@@ -39,7 +39,7 @@ int raw_tracepoint_sys_enter(struct bpf_raw_tracepoint_args *ctx)
   const char *pathname;
   u32 map_id;
   int res;
-	struct event_t *event;
+  struct event_t *event;
 
   if (syscall_id != __NR_openat)
     return 0;
@@ -64,7 +64,7 @@ int raw_tracepoint_sys_enter(struct bpf_raw_tracepoint_args *ctx)
 
   event->pid = bpf_get_current_pid_tgid();
   bpf_printk("open_at: %d %s\n", sizeof(*event), event->str);
-	bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, event, sizeof(*event));
+  bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, event, sizeof(*event));
 
   return 0;
 }
